de627530b0dc36b3f4399c1c5217713e53ab3546
PJMantoss <joelptoss@gmail.com>
2019-10-01 18:27:50 +0100

    Add ReactCSSTransition

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 function Overlay(props){
   return (
@@ -38,10 +38,36 @@ class App extends React.Component {
       discount: 5
     }
   }
-  return (
-    <div className="App">
-    </div>
-  );
+
+  render(){
+
+    let overlay, container;
+    if(this.state.mounted){
+      overlay = (
+        <Overlay image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/jj-2.jpg" />
+      );
+      container = (
+        <Container>
+          <ImagePreview price={this.state.price} duration={this.state.duration} people={this.state.people} image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/jj-2.jpg" />
+          <Checkout duration={this.state.duration} discount={this.state.discount} tax={this.state.tax} price={this.state.price} onSubmit={this.handleSubmit} />
+        </Container>
+      );
+    }
+
+
+    return (
+      <div className="App">
+        <ReactCSSTransitionGroup transitionName="overlay" transitionEnterTimeout={500} transitionLeaveTimeout={300}>
+          {overlay}
+        </ReactCSSTransitionGroup>
+        <ReactCSSTransitionGroup transitionName="container" transitionEnterTimeout={500} transitionLeaveTimeout={300}>
+          {container}
+        </ReactCSSTransitionGroup>
+        <Header onChange={this.handleChange} />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
