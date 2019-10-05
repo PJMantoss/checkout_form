c515134c63397a5f24370f14eb5123603d3d6e21
PJMantoss <joelptoss@gmail.com>
2019-10-05 20:17:49 +0100

    Update PaymentForm

--- src/components/checkoutArea/PaymentFormComponents.js
+++ src/components/checkoutArea/PaymentFormComponents.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function BuiltInput(props){
+function BasicInput(props){
     return(
         <div className="BasicInput">
             <label htmlFor={props.name}>{props.label}</label>
@@ -33,18 +33,54 @@ function ExpiryDate(props){
                         <option value="">November</option>
                         <option value="">December</option>
                     </select>
+                    <select name="" id="">
+                        <option value="">2016</option>
+                        <option value="">2017</option>
+                        <option value="">2018</option>
+                        <option value="">2019</option>
+                        <option value="">2020</option>
+                        <option value="">2021</option>
+                    </select>
                 </div>
             </div>
         </div>
     )
 }
 
-function PaymentFormComponents(){
-    return (
-        <div>
-            
+function CheckoutButton(props){
+    return(
+        <div className="CheckoutButton">
+            <button>Book Securely</button>
+            <span>
+                <i className="fa fa-fw fa-lock"></i> 
+                Your credit card information is encrypted
+            </span>
         </div>
     )
 }
 
-export default PaymentFormComponents
+function PaymentFormComponents(props){
+    return (
+        <div className="PaymentForm">
+            <form onSubmit={props.onSubmit}>
+                <div className="Title">Payment information</div>
+                <BasicInput 
+                   name="name" 
+                   label="Name on credit card" 
+                   type="text" 
+                   placeholder="Joel Mantoss" 
+                />
+                <BasicInput 
+                   name="card" 
+                   label="Credit card number" 
+                   type="number" 
+                   placeholder="0000 0000 0000 0000" 
+                />
+                <ExpiryDate />
+                <CheckoutButton />
+            </form>
+        </div>
+    )
+}
+
+export default PaymentFormComponents;
