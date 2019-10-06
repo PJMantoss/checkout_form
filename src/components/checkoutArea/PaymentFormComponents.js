a03bbe64d0ee06ee1b145b5f982ffec82631bbe8
PJMantoss <joelptoss@gmail.com>
2019-10-06 14:41:17 +0100

    Update ImagePreview and PaymentForm

--- src/components/checkoutArea/PaymentFormComponents.js
+++ src/components/checkoutArea/PaymentFormComponents.js
@@ -34,12 +34,12 @@ function ExpiryDate(props){
                         <option value="">December</option>
                     </select>
                     <select name="" id="">
-                        <option value="">2016</option>
-                        <option value="">2017</option>
-                        <option value="">2018</option>
                         <option value="">2019</option>
                         <option value="">2020</option>
                         <option value="">2021</option>
+                        <option value="">2022</option>
+                        <option value="">2023</option>
+                        <option value="">2024</option>
                     </select>
                 </div>
             </div>
--- src/components/imagePreviewArea/ImagePreview.js
+++ src/components/imagePreviewArea/ImagePreview.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import * as WorkspaceComponents from './WorkspaceComponents';
+import WorkspaceComponents from './WorkspaceComponents';
 
 function ImagePreview(props) {
     return (
         <div className="ImagePreview" style={{'backgroundImage': 'url('+ props.image +')'}}>
             <div className="WorkspaceOverview">
-            <Workspace.Information name="Coworking Space, Holland" price={props.price} duration="1" />
-            <Workspace.Meta people={props.people} />
+            <WorkspaceComponents 
+               name="Coworking Space, Holland" 
+               price={props.price} duration="1" 
+               people={props.people} />
         </div>
         </div>
     )
